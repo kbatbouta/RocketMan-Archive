380461b203637507bc0e7e31f125c78d82c5f3b8
Karim Batbouta <kbatbouta66@gmail.com>
2020-10-26 13:48:56 -0700

    - Added the legacy files (by drowsy corgi).

---
+++ RocketMan/Legacy/Model/AbstractStatRequestModel.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Verse;
+
+namespace RocketMan
+{
+    public class AbstractStatRequestModel : EqualityComparer<AbstractStatRequestModel>
+    {
+        private Def Def { get; set; }
+
+        private Def Stuff { get; set; }
+
+        public override bool Equals(AbstractStatRequestModel x, AbstractStatRequestModel y)
+        {
+            if (x.Def == y.Def && x.Stuff == y.Stuff)
+                return true;
+
+            return false;
+        }
+
+        public override int GetHashCode(AbstractStatRequestModel obj)
+        {
+            return Gen.HashCombineInt(obj.Def.shortHash, obj.Stuff?.shortHash ?? 0);
+        }
+    }
+}
---
+++ RocketMan/Legacy/Model/GridTempModel.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Verse;
+
+namespace RocketMan
+{
+    public struct GridTempModel
+    {
+        public IntVec3 Cell;
+
+        public Map Map;
+
+        public GridTempModel(IntVec3 cell, Map map)
+        {
+            Cell = cell;
+            Map = map;
+        }
+    }
+
+    public class GridTempModelComparer : EqualityComparer<GridTempModel>
+    {
+        public static GridTempModelComparer Instance = new GridTempModelComparer();
+
+        private GridTempModelComparer()
+        {
+        }
+
+        #region Overrides of EqualityComparer<GridTempModel>
+
+        public override bool Equals(GridTempModel x, GridTempModel y)
+        {
+            return x.Cell == y.Cell && x.Map == y.Map;
+        }
+
+        public override int GetHashCode(GridTempModel obj)
+        {
+            unchecked
+            {
+                int hash;
+                hash = HashUtility.HashOne(obj.Cell.GetHashCode());
+                hash = HashUtility.HashOne(obj.Map.GetHashCode(), hash);
+
+                return hash;
+            }
+        }
+
+        #endregion
+    }
+}
