1f6197899319d0181d502b1a76b14941ebb00dee
Karim Batbouta <kbatbouta66@gmail.com>
2021-05-12 11:49:50 -0700

    Fixed a Log.Error_Patch error.

--- Rocketeer/Core/Patches/Log_Patch.cs
+++ Rocketeer/Core/Patches/Log_Patch.cs
@@ -8,22 +8,22 @@ using Verse;
 
 namespace Rocketeer
 {
-    public static class Log_Error_Patch
-    {
-        [Main.OnInitialization]
-        public static void OnDefsLoaded()
-        {
-            try
-            {
-                Log.Message("ROCKETEER: PATCHING STARTED!");
-                Finder.harmony.Patch(AccessTools.Method(typeof(Log), "Error"), new HarmonyMethod(AccessTools.Method(typeof(Log_Error_Patch), "Prefix")));
-            }
-            catch (Exception er)
-            {
-                Log.Error($"ROCKETEER: PATCHING FAILED! {er}");
-            }
-        }
-
-        public static void Prefix(string text) => ErrorUtility.Process(text, new StackTrace(2, fNeedFileInfo: true));
-    }
+    //public static class Log_Error_Patch
+    //{
+    //    [Main.OnInitialization]
+    //    public static void OnDefsLoaded()
+    //    {
+    //        try
+    //        {
+    //            Log.Message("ROCKETEER: PATCHING STARTED!");
+    //            Finder.harmony.Patch(AccessTools.Method(typeof(Log), "Error"), new HarmonyMethod(AccessTools.Method(typeof(Log_Error_Patch), "Prefix")));
+    //        }
+    //        catch (Exception er)
+    //        {
+    //            Log.Error($"ROCKETEER: PATCHING FAILED! {er}");
+    //        }
+    //    }
+    //
+    //    public static void Prefix(string text) => ErrorUtility.Process(text, new StackTrace(2, fNeedFileInfo: true));
+    //}
 }
