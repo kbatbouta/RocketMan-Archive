2de1c909fa0ccf26f2c6c9d68f63021f97aa2026
Karim Batbouta <kbatbouta66@gmail.com>
2021-05-27 11:49:06 -0700

    Initial working version of Gagarin

--- Gagarin/Core/DefXmlHelper.cs
+++ Gagarin/Core/DefXmlHelper.cs
@@ -11,22 +11,15 @@ namespace Gagarin.Core
     public static class DefXmlHelper
     {
         private static HashSet<XmlElement> skipSet = new HashSet<XmlElement>();
-
         private static Dictionary<string, XmlElement> namelookup = new Dictionary<string, XmlElement>();
-
-        private static Dictionary<string, XmlElement> defnamelookup = new Dictionary<string, XmlElement>();
-
-        private static string report = string.Empty;
 
         public static void Clear()
         {
-            defnamelookup.Clear();
             namelookup.Clear();
         }
 
         public static HashSet<XmlElement> FindDuplicates(XmlNodeList nodes)
         {
-            report = string.Empty;
             for (int i = 0; i < nodes.Count; i++)
             {
                 if (nodes[i] is XmlElement node)
@@ -39,34 +32,13 @@ namespace Gagarin.Core
                     {
                         TryRegisterNamed(node, node.GetAttribute("Name"));
                     }
-                    if (!node.HasChildNodes)
-                    {
-                        continue;
-                    }
-                    XmlNodeList children = node.ChildNodes;
-                    for (int j = 0; j < children.Count; j++)
-                    {
-                        if (children[j] is XmlElement sub)
-                        {
-                            if (sub?.IsEmpty ?? true)
-                            {
-                                continue;
-                            }
-                            if (sub.Name == "defName")
-                            {
-                                TryRegisterDefName(node, node.Name + "$" + sub.InnerText);
-                            }
-                        }
-                    }
                 }
             }
-            Log.Message($"GAGARIN: FindDuplicates <color=red>report</color>\n{report}");
             return skipSet;
         }
 
         private static void TryRegisterNamed(XmlElement node, string name)
         {
-            report += $"{skipSet.Count}: TryRegisterNamed {name}";
             if (skipSet.Contains(node))
             {
                 return;
@@ -76,20 +48,6 @@ namespace Gagarin.Core
                 skipSet.Add(other);
             }
             namelookup[name] = node;
-        }
-
-        private static void TryRegisterDefName(XmlElement node, string name)
-        {
-            report += $"{skipSet.Count}: TryRegisterNamedDef {name}\n";
-            if (skipSet.Contains(node))
-            {
-                return;
-            }
-            if (defnamelookup.TryGetValue(name, out XmlElement other))
-            {
-                skipSet.Add(other);
-            }
-            defnamelookup[name] = node;
         }
     }
 }
